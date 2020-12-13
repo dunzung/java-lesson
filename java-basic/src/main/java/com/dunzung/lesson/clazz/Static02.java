7d0733d47dac3507a3a22b9bd37034778ef308aa
dunzung <762718879@qq.com>
2020-12-13 15:55:26 +0800

    update

---
+++ java-basic/src/main/java/com/dunzung/lesson/clazz/Static02.java
@@ -0,0 +1,14 @@
+package com.dunzung.lesson.clazz;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Static02 {
+
+    static {
+        i = 9;
+        System.out.println(i);
+    }
+
+    static int i = 10;
+
+}
